5aba8d23c34aa4371a3573ba5830e5918cdfe0fc
Yen04 <elvisyen0727@gmail.com>
2026-07-23 12:36:49 +0800

    Type renderer integration bridge

--- app/src/renderer/env.d.ts
+++ app/src/renderer/env.d.ts
@@ -1,6 +1,7 @@
 import type { MelDesktopApi } from '../shared/contracts'
 import type { CustomModelApi } from '../shared/custom-model-contracts'
 import type { DiagnosticsApi } from '../shared/diagnostics-contracts'
+import type { IntegrationApi } from '../shared/integration-contracts'
 import type { ReportTemplateApi } from '../shared/template-contracts'
 
 declare global {
@@ -9,6 +10,7 @@ declare global {
     melDiagnostics: DiagnosticsApi
     melTemplates: ReportTemplateApi
     melCustomModels: CustomModelApi
+    melIntegrations: IntegrationApi
   }
 }
 
