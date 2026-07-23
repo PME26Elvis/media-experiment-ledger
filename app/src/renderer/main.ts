8f17d618ff6ef2d248ec28117c1fba60b7a3e9cd
Yen04 <elvisyen0727@gmail.com>
2026-07-23 13:03:25 +0800

    Install packaged renderer smoke audit

--- app/src/renderer/main.ts
+++ app/src/renderer/main.ts
@@ -8,5 +8,7 @@ import App from './App.vue'
 import { vuetify } from './plugins/vuetify'
 import { router } from './router'
 import { i18n } from './i18n'
+import { installSmokeAudit } from './smoke-audit'
 
+installSmokeAudit(router, i18n)
 createApp(App).use(createPinia()).use(VueQueryPlugin).use(router).use(i18n).use(vuetify).mount('#app')
